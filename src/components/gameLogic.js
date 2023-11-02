1ebac1ae4db2199f63f20994c41bd2b7a2d165b4
Godwin Emeribe <godwinemeribe23@gmail.com>
2023-11-02 10:32:50 +0100

    Wrote down features I want the game to have and how I want it to work

---
+++ src/components/gameLogic.js
@@ -0,0 +1,8 @@
+const getCardNumberByLevel = levelNum => levelNum * 6;
+
+export const DifficultyLevel = function (array, difficulty, cardNumber) {
+    if (difficulty.toLowerCase() === 'easy') {
+
+    }
+}();
+
